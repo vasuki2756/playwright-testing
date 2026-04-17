4606a992dce16a912a3e1f4850f73cd237d59a23
vasuki <vasukig@karunya.edu.in>
2026-04-17 10:23:42 +0530

    after corrected location

--- playwright-project/playwright.config.js
+++ playwright-project/playwright.config.js
@@ -25,11 +25,12 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+  
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+  use: {
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     trace: 'on-first-retry',
   },
 
